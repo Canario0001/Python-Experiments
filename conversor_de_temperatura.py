3d2f0b695754ad808fb2b80fa2e2eeab52e558e2
Canário (or Canary) <81337238+Canario0001@users.noreply.github.com>
2022-01-29 14:49:59 -0300

    Create conversor_de_temperatura.py

---
+++ conversor_de_temperatura.py
@@ -0,0 +1,5 @@
+print('Olá! Sou um programa para converter °C em °F!\nPs: troque a vírgula por um ponto.')
+tempc = float(input ('Digite aqui a temperatura que você desejar em °C: '))
+tempf = (tempc*9/5)+32
+print('{}°C equivale à {}°F.'.format(tempc,tempf))
+#
